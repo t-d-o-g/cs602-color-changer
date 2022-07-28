f81141d07072236b4b85e5b9c85f1c7dec78b1a0
t-d-o-g <trc229@gmail.com>
2022-07-28 07:49:30 -0400

    Add slider to change color

--- src/main/java/ColorChanger.java
+++ src/main/java/ColorChanger.java
@@ -1,8 +1,12 @@
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.control.Slider;
+import javafx.scene.transformk.Translate;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,19 +14,28 @@ import java.io.IOException;
 
 public class ColorChanger extends Application {
 
-    private static Scene scene;
+	private static Scene scene;
 
-    @Override
-    public void start(Stage stage) throws IOException {
-				Rectangle rect = new Rectangle(50, 50, 300, 100);
+	public void start(Stage stage) throws IOException {
+		Slider slider = new Slider(0, 255, 0);
+		Rectangle rect = new Rectangle(50, 50, 300, 100);
 
-				Group root = new Group(rect);
-        scene = new Scene(root, 640, 480, Color.WHITE);
-        stage.setScene(scene);
-        stage.show();
-    }
+		slider.valueProperty().addListener(new ChangeListener<Number>() {
+			public void changed(ObservableValue <?extends Number>observable, Number oldValue, Number newValue) {
+				System.out.println("RGB Color Value: " + newValue.intValue());
+				rect.setFill(Color.rgb(newValue.intValue(), newValue.intValue(), newValue.intValue()));
+			}
+		});
+		
+		Group root = new Group(rect);
+		scene = new Scene(root, 400, 200, Color.WHITE);
+		stage.setScene(scene);
+		stage.setTitle("Color Changer");
+		root.getChildren().add(slider);
+		stage.show();
+	}
 
-    public static void main(String[] args) {
-        launch();
-    }
+	public static void main(String[] args) {
+			launch();
+	}
 }
